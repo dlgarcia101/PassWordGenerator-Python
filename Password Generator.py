5176f9a3c78611c535ba0bf37bb3ff1ff210434b
dlgarcia101 <dlgarcia101@gmail.com>
2025-11-12 20:43:32 -0500

    Update of 1st 3 characters
    update to password 1st three chrac are UpperCase(alpha characters)

--- Password Generator.py
+++ Password Generator.py
@@ -7,17 +7,26 @@ import random , string
 
 symbols = ['@','!','&','%','#','*']
 alphabet =  string.ascii_lowercase + string.ascii_uppercase + string.digits + random.choice(symbols)
+firstThreeCharc =  ''.join(random.choice(string.ascii_uppercase) for i in range(3))
 
-passwordLenght = 12
+
 
 
 def generatePassword(length):
-    randomPassWord = ''.join(random.choice(alphabet)for i in range(length))
+
+    randomPassWord = ''.join(random.choice(alphabet) for i in range(length))
+    print('PW Lenght:' + str(len(randomPassWord)))
+    print('randomPW:' + randomPassWord)
+    randomPassWord = randomPassWord[3:]
+    
+    randomPassWord  = firstThreeCharc + randomPassWord
+
+
     return randomPassWord
+    
 
 
-
-myPassword = generatePassword(12)
+myPassword = generatePassword(8)
 print('Password:'+ myPassword)
 
 
