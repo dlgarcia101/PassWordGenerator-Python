e01220bd337bec069a648b92f0bc630cda752b11
dlgarcia101 <dlgarcia101@gmail.com>
2026-01-07 09:22:21 -0500

    Update to the 1st 3 chrc being Upper

--- Password Generator.py
+++ Password Generator.py
@@ -6,24 +6,27 @@ This utilizes string manipulation and the random module. """
 import random , string
 
 symbols = ['@','!','&','%','#','*']
-alphabet =  string.ascii_lowercase + string.ascii_uppercase + string.digits + random.choice(symbols)
+alphabet =  string.ascii_lowercase + string.ascii_uppercase + random.choice(symbols)
 firstThreeCharc =  ''.join(random.choice(string.ascii_uppercase) for i in range(3))
-
 
 
 
 def generatePassword(length):
 
-    randomPassWord = ''.join(random.choice(alphabet) for i in range(length))
-    print('PW Lenght:' + str(len(randomPassWord)))
-    print('randomPW:' + randomPassWord)
-    randomPassWord = randomPassWord[3:]
+    while True:
+        randomPassWord = ''.join(random.choice(alphabet) for i in range(length))
+        print('PW Lenght:' + str(len(randomPassWord)))
+        print('IntialRandomPW:' + randomPassWord)
+        randomPassWord = randomPassWord[3:]
     
-    randomPassWord  = firstThreeCharc + randomPassWord
+        randomPassWordFinal  = firstThreeCharc + randomPassWord
 
-
-    return randomPassWord
-    
+        for char in symbols:
+            if(char in randomPassWordFinal):
+                return randomPassWordFinal
+            else:
+                print('symbol not found:'+ char)
+                
 
 
 myPassword = generatePassword(8)
